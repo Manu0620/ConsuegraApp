3b994c2bcc0c30d347fb5f630842c14d4e27e126
Manu0620 <maderamanuel25@gmail.com>
2024-10-11 10:04:01 -0400

    Send Email

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -20,24 +20,3 @@ export const GET = async (req: NextRequest) => {
       return NextResponse.json({ error: 'An error occurred while fetching products' }, { status: 500 });
   }
 };
-
-export const GETbyId = async (productId: string) => {
-   try {
-      const result = await fetch(`https://fakestoreapi.com/products/${productId}`, {
-         headers: {
-         'Content-Type': 'application/json',
-         },
-         method: 'GET',
-      });
-   
-      if (!result.ok) {
-         return NextResponse.json({ error: 'Failed to fetch product' }, { status: 500 });
-      }
-   
-      const product = await result.json();
-      return NextResponse.json({ product });
-   } catch (error) {
-      console.error('Error fetching product:', error);
-      return NextResponse.json({ error: 'An error occurred while fetching product' }, { status: 500 });
-   }
-};
